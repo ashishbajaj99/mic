1cbaf21e376831766f4265a752fea841addf253d
Ashish Bajaj <bajaj.ashish@gmail.com>
2016-02-07 10:16:55 +0530

    bugfix: handle stderr of child_process correctly

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,16 @@
 var mic = require('../index.js');
 var fs = require('fs');
 
-var micInstance = mic({ 'rate': '16000', 'channels': '1', 'debug': true, 'exitOnSilence': 6 });
+var micInstance = mic({ 'rate': '16000', 'channels': '1', 'debug': false });
 var micInputStream = micInstance.getAudioStream();
 
 var outputFileStream = fs.WriteStream('output.raw');
 
 micInputStream.pipe(outputFileStream);
 
+var chunkCounter = 0;
 micInputStream.on('data', function(data) {
-    console.log("Recieved Input Stream: " + data.length);
+        console.log("Recieved Input Stream of Size %d: %d", data.length, chunkCounter++);
 });
 
 micInputStream.on('error', function(err) {
